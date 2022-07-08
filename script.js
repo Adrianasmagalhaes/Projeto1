d207aab50252afe7ff679ba53fff64fb6fd6a59f
Adriana <a.s.m27267@gmail.com>
2022-07-07 21:07:46 -0300

    Aplicando json

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 //Quando não declaro o tipo de método usando o fetch, ele entende que é GET(TRAZ INFORMAÇÃOES)
-const responce = fetch ('https://jsonplaceholder.typicode.com/posts').then(res => {
+const responce = fetch('https://jsonplaceholder.typicode.com/users').then(responce => {
     //Promisse que trata o sucesso é o metodo then(),o parâmetro res recebe o retorno da funçao anterios
-    console.log(res.json())
+    console.log(responce);
+    return responce.json();
+
 })
-//Promisse que trata o erro é método catch()
-.catch(error =>{
-    console.log(`Deu erro na busca ${error}`)
-} )
+    //Para selecionar o usuario[0] traz na ondem o usuario escolido
+    .then(data => {
+        console.log(data)
+        //Usamos para inteirar (percorrer o array), o map()
+        //que é um método para percorrer todo o array como um for
+        data.map(user => {
+            console.log(user)
+            console.log(`Nome: ${user.name}, email: ${user.email}`)
+        })
+    })
+    //Promisse que trata o erro é método catch()
+    .catch(error => {
+        console.log(`Deu erro na busca ${error}`)
+    })
 // console.log(responce)
